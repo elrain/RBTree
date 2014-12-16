0f35814991376cb847aa0e65a2f04476cbab6d52
elrain <denis.gusher@gmail.com>
2014-12-16 22:42:22 +0200

    removed comparator

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,39 +3,40 @@ package main;
 import objects.Student;
 import util.RBTree;
 
+import java.util.Random;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		RBTree<Student> rbTree = new RBTree<Student>(Student.comparatorById);
+		RBTree<Student> rbTree = new RBTree<Student>();
 
-		rbTree.add(10, new Student(4, "0001", "Student 4"));
-		rbTree.add(85, new Student(44, "0103", "Student 2"));
-		rbTree.add(15, new Student(8, "0004", "Student 3"));
-		rbTree.add(70, new Student(22, "0002", "Student 1"));
-		rbTree.add(20, new Student(8, "0004", "Student 3"));
-		rbTree.add(60, new Student(8, "0004", "Student 3"));
-		rbTree.add(30, new Student(8, "0004", "Student 3"));
-		rbTree.add(50, new Student(8, "0004", "Student 3"));
-		rbTree.add(40, new Student(8, "0004", "Student 3"));
-//
-////		for (int index = 0; index < rbTree.size() ; ++index) {
-////			System.out.println(rbTree.get(index).getStudentId() + " "
-////					+ rbTree.get(index).getStudentGroup());
-////		}
-//
-//		 TreeSet<Integer> treeSet = new TreeSet<Integer>();
-//		
-//		 treeSet.add(0);
-//		 treeSet.add(2);
-//		 treeSet.add(5);
-//		 treeSet.add(6);
-//		 treeSet.add(22);
-		int i=0;
-		++i;
-		
+		Random r = new Random();
+
+//		rbTree.add(10, new Student(4, "0001", "Student 4"));
+//		rbTree.add(85, new Student(44, "0103", "Student 2"));
+//		rbTree.add(15, new Student(8, "0004", "Student 3"));
+//		rbTree.add(70, new Student(22, "0002", "Student 1"));
+//		rbTree.add(20, new Student(8, "0004", "Student 3"));
+//		rbTree.add(60, new Student(8, "0004", "Student 3"));
+//		rbTree.add(30, new Student(8, "0004", "Student 3"));
+//		rbTree.add(50, new Student(8, "0004", "Student 3"));
+//		rbTree.add(40, new Student(8, "0004", "Student 3"));
+
+		rbTree.add(new Student(10, "0001", "Student 4"));
+		rbTree.add(new Student(85, "0103", "Student 2"));
+		rbTree.add(new Student(15, "0004", "Student 3"));
+		rbTree.add(new Student(70, "0002", "Student 1"));
+		rbTree.add(new Student(20, "0004", "Student 3"));
+		rbTree.add(new Student(60, "0004", "Student 3"));
+		rbTree.add(new Student(30, "0004", "Student 3"));
+		rbTree.add(new Student(50, "0004", "Student 3"));
+		rbTree.add(new Student(40, "0004", "Student 3"));
+
+//		System.out.println(rbTree.get(5).getStudentId() + "\n\n");
+
 //		rbTree.preOrderTravers();
 //		System.out.println("\n");
 		rbTree.inOrderTravers();
--- src/objects/Student.java
+++ src/objects/Student.java
@@ -1,56 +1,44 @@
 package objects;
 
-import java.util.Comparator;
+public class Student implements Comparable<Student> {
+    private int studentId;
+    private String studentGroup;
+    private String studentName;
 
-public class Student{
-	private int studentId;
-	private String studentGroup;
-	private String studentName;
-	
-	public Student(int studentId, String studentGroup, String studentName)
-	  {
-	    this.studentId = studentId;
-	    this.studentGroup = studentGroup;
-	    this.studentName = studentName;  
-	  }
-	
-	public int getStudentId() {
-		return studentId;
-	}
-	public void setStudentId(int studentId) {
-		this.studentId = studentId;
-	}
-	public String getStudentGroup() {
-		return studentGroup;
-	}
-	public void setStudentGroup(String studentGroup) {
-		this.studentGroup = studentGroup;
-	}
-	public String getStudentName() {
-		return studentName;
-	}
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
-	
-	public static Comparator<Object> comparatorById = new Comparator<Object>() {
-		
-		@Override
-		public int compare(Object o1, Object o2) {
-			int id1 = ((Student)o1).getStudentId();
-			int id2 = ((Student)o2).getStudentId();
-			return id1 - id2;
-		}
-	};
-	
-	public static Comparator<Object> comparatorByGroup = new Comparator<Object>() {
-		
-		@Override
-		public int compare(Object o1, Object o2) {
-			String group1 = ((Student)o1).getStudentGroup();
-			String group2 = ((Student)o2).getStudentGroup();
-			return group1.compareTo(group2);
-		}
-	};
-	
+    public Student(int studentId, String studentGroup, String studentName) {
+        this.studentId = studentId;
+        this.studentGroup = studentGroup;
+        this.studentName = studentName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentGroup() {
+        return studentGroup;
+    }
+
+    public void setStudentGroup(String studentGroup) {
+        this.studentGroup = studentGroup;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        int id1 = this.getStudentId();
+        int id2 = o.getStudentId();
+        return id1 - id2;
+    }
 }
